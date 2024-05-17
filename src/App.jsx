c03dd10088fbbbeba835fbf426c551dc0ddbfbd0
Xiyuan Liu <xliu1@tcd.ie>
2024-05-17 23:17:09 +0100

    Design Sidebar (Part I)
    Create Layouts folder containing PageLayout folder containing PageLayout.jsx, rafce, takes children as prop.
    
    useLocation hook gets the current pathname.
    
    PageLayout.jsx is a Flex, possibly rendering a Sidebar in a Box. Another Box containing {children} is used for the main content area.
    
    Create Sidebar folder containing Sidebar.jsx, rafce, import
    
    Wrap Routes in PageLayout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage"
 import AuthPage from "./pages/AuthPage/AuthPage"
+import PageLayout from "./Layouts/PageLayout/PageLayout"
 
 function App() {
-  return <>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/auth" element={<AuthPage/>}/>
-    </Routes>
-  </>
+  return (
+    <PageLayout>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/auth" element={<AuthPage/>}/>
+      </Routes>
+    </PageLayout>
+  )
 }
 
 export default App
