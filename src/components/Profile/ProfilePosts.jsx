d8141b20c45053d7b778967e4452bb3c33415f6a
Xiyuan Liu <xliu1@tcd.ie>
2024-05-23 00:54:29 +0100

    Design Profile Page (Part III)
    ProfileTabs.jsx is a Flex containing three Flexes. Each Flex contains an icon and Text for posts, saved and likes.
    
    The useEffect hook in ProfilePosts.jsx runs when the component mounts. It returns a Grid with templateColumns={{sm:"repeat(1, 1fr)", md:"repeat(3, 1fr)"}}.
    
    Skeleton loaders are created by mapping over an array of six indices.
    
    Each index generates a VStack component with a Skeleton inside.
    
    Each Skeleton wraps a Box component with h={300}.

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,9 +1,24 @@
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+
 const ProfilePosts = () => {
-  return (
-    <div>
-      ProfilePosts
-    </div>
-  )
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
+
+  return <Grid templateColumns={{sm:"repeat(1, 1fr)", md:"repeat(3, 1fr)"}} gap={1} columnGap={1}>
+    {isLoading && [0, 1, 2, 3, 4, 5].map((_, idx) => (
+      <VStack key={idx}>
+        <Skeleton w={"full"}>
+          <Box h={300}>contents wrapped</Box>
+        </Skeleton>
+      </VStack>
+    ))}
+  </Grid>
 }
 
 export default ProfilePosts
--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,9 +1,21 @@
+import { Flex, Text } from "@chakra-ui/react"
+import { BsBookmark, BsGrid3X3, BsSuitHeart } from "react-icons/bs"
+
 const ProfileTabs = () => {
-  return (
-    <div>
-      ProfileTabs
-    </div>
-  )
+  return <Flex w={"full"} justifyContent={"center"} gap={{base:4, sm:10}} textTransform={"uppercase"} fontWeight={"bold"}>
+    <Flex borderTop={"1px solid white"} alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+      <BsGrid3X3/>
+      <Text fontSize={12} display={{base:"none", sm:"block"}}>Posts</Text>
+    </Flex>
+    <Flex alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+      <BsBookmark/>
+      <Text fontSize={12} display={{base:"none", sm:"block"}}>Saved</Text>
+    </Flex>
+    <Flex alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+      <BsSuitHeart />
+      <Text fontSize={12} display={{base:"none", sm:"block"}}>Likes</Text>
+    </Flex>  
+  </Flex>
 }
 
 export default ProfileTabs
