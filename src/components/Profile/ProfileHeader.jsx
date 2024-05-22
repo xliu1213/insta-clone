1b83e94e4090bc40cbf3b8b1e0c398411568be20
Xiyuan Liu <xliu1@tcd.ie>
2024-05-22 17:46:09 +0100

    Design Profile Page (Part II)
    ProfilePage.jsx returns a Container containing two Flex. The first Flex contains a ProfileHeader and the second Flex contains a ProfileTabs and ProfilePosts (all stacked vertically).
    
    ProfileHeader.jsx returns a Flex containing an AvatarGroup and a VStack. This Flex has different directions in base and sm.
    
    The AvatarGroup contains an Avatar and the VStack contains two Flex (user name + edit profile button) (number of posts, followers and following)
    
    and two Text (name and bio).

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,9 +1,26 @@
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+
 const ProfileHeader = () => {
-  return (
-    <div>
-      ProfileHeader
-    </div>
-  )
+  return <Flex gap={{base:4,sm:10}} py={10} direction={{base:"column", sm:"row"}}>
+    <AvatarGroup size={{base:"xl",md:"2xl"}} mx={"auto"}>
+      <Avatar name="Default profile pic" src="/profilepic.png" alt="Default profile picture logo"/>
+    </AvatarGroup>
+    <VStack alignItems={"start"} flex={1}>
+      <Flex gap={4} direction={{base:"column", sm:"row"}} alignItems={"center"} w={"full"}>
+          <Text fontSize={{base:"sm",md:"lg"}}>xiyuan</Text>
+          <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
+            Edit Profile
+          </Button>
+      </Flex>
+      <Flex gap={{base:2, sm:4}} fontSize={{base:"xs", md:"sm"}}>
+        <Text><Text as={"span"} fontWeight={"bold"} mr={1}>4</Text> Posts</Text>
+        <Text><Text as={"span"} fontWeight={"bold"} mr={1}>149</Text> Followers</Text>
+        <Text><Text as={"span"} fontWeight={"bold"} mr={1}>175</Text> Following</Text>
+      </Flex>
+      <Text fontSize={"sm"} fontWeight={"bold"}>Xiyuan</Text>
+      <Text fontSize={"sm"}>Created an insta-clone using React and Vite.</Text>
+    </VStack>
+  </Flex>
 }
 
 export default ProfileHeader
