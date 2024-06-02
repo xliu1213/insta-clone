60780e16a1f1210e7f0e316c7ad497c0340de398
Xiyuan Liu <xliu1@tcd.ie>
2024-06-02 14:53:04 +0100

    Update AuthForm.jsx (Part II)
    Signup.jsx uses two state variables: inputs and showPassword.
    
    inputs is an object holding the form input values. showPassword is a Boolean that toggles the visibility of the password input.
    
    Signup.jsx returns 3 Input components and one InputGroup.
    
    The InputGroup contains an Input and an InputRightElement.
    
    The InputRightElement contains a Button.

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,9 +1,34 @@
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { useState } from 'react'
+
 const Signup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [inputs, setInputs] = useState({
+    email: "",
+    username: "",
+    fullName: "",
+    password: ""
+  })
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  return <>
+    <Input placeholder="Email" fontSize={14} type="email" value={inputs.email} size={"sm"}
+      onChange={(e) => setInputs({...inputs, email: e.target.value})} />
+    <Input placeholder="Username" fontSize={14} type="text" value={inputs.username} size={"sm"}
+      onChange={(e) => setInputs({...inputs, username: e.target.value})} />
+    <Input placeholder="Full Name" fontSize={14} type="text" value={inputs.fullName} size={"sm"}
+      onChange={(e) => setInputs({...inputs, fullName: e.target.value})} />
+    <InputGroup>
+      <Input placeholder='Password' fontSize={14} type={showPassword ? "text" : "password"} value={inputs.password} size={"sm"}
+        onChange={(e) => setInputs({...inputs, password: e.target.value})}/>
+      <InputRightElement h={"full"}>
+        <Button variant={"ghost"} size={"sm"} onClick={() => setShowPassword(!showPassword)}>
+          {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  </>
 }
 
 export default Signup
