c9f3c22d0b60eab88aa8fc73d5cbf728373d6b86
Xiyuan Liu <xliu1@tcd.ie>
2024-06-04 19:42:45 +0100

    useAuthState Example
    useLogout.js retrieves the logoutUser function from the state store.
    
    logoutUser is called so the global state is updated to reflect the logout.
    
    PageLayout.js uses useAuthState to get the current authentication state.
    
    The canRenderSidebar constant determines if the sidebar should be rendered.

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,16 +1,21 @@
 import { Box, Flex } from "@chakra-ui/react"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import { useLocation } from "react-router-dom"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "../../firebase/firebase"
 
 // instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component
 // and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage.
 
 const PageLayout = ({children}) => {
   const {pathname} = useLocation()
+  const [user, loading, error] = useAuthState(auth)
+  const canRenderSidebar = pathname !== "/auth" && user
+
   return (
     <Flex>
       {/* sidebar on the left */}
-      {pathname !== '/auth' ? (
+      {canRenderSidebar ? (
         <Box w={{base:"70px", md:"240px"}}>
           <Sidebar/>
         </Box>
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,16 +1,18 @@
 import { useSignOut } from 'react-firebase-hooks/auth'
 import { auth } from '../firebase/firebase'
 import useShowToast from './useShowToast'
+import useAuthStore from '../store/authStore'
 
 const useLogout = () => {
   const showToast = useShowToast()
   const [signOut, isLoggingOut, error] = useSignOut(auth)
+  const logoutUser = useAuthStore(state => state.logout)
 
   const handleLogout = async () => {
     try {
       await signOut()
       localStorage.removeItem("user-info")
-      console.log("logged out")
+      logoutUser()
     } catch (error) {
       showToast("Error", error.message, "error")
     }
