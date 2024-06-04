e8f363c54b79d7fc0b58b2da58874cd5f0976bfd
Xiyuan Liu <xliu1@tcd.ie>
2024-06-04 20:07:29 +0100

    Create Navbar (Part I)
    Create Navbar folder containing Navbar.jsx, rafce.
    
    PageLayout.jsx conditionally renders a navbar when the user is not authenticated, the page is not /auth and loading is complete.
    
    It uses a column layout when the navbar is rendered and a row layout otherwise.
    
    Navbar.jsx is a Container containing a Flex containing an Image (Instagram logo) and a Flex (Login and Signup buttons).
    
    The Flex contains 2 Link components each containing a Button.

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../components/Sidebar/Sidebar"
 import { useLocation } from "react-router-dom"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from "../../firebase/firebase"
+import Navbar from "../../components/Navbar/Navbar"
 
 // instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component
 // and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage.
@@ -11,18 +12,20 @@ const PageLayout = ({children}) => {
   const {pathname} = useLocation()
   const [user, loading, error] = useAuthState(auth)
   const canRenderSidebar = pathname !== "/auth" && user
+  const canRenderNavbar = !user && !loading && pathname !== "/auth"
 
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/* sidebar on the left */}
       {canRenderSidebar ? (
         <Box w={{base:"70px", md:"240px"}}>
           <Sidebar/>
         </Box>
       ) : null}
-      
+      {/* Navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
       {/* the page content on the right */}
-      <Box flex={1} w={{base:"calc(100% - 70px)", md: "calc(100% - 240px)"}}>
+      <Box flex={1} w={{base:"calc(100% - 70px)", md: "calc(100% - 240px)"}} mx={"auto"}>
         {children}
       </Box>
     </Flex>
