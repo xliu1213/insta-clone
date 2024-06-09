17f9105391f440bb66191ea5edd321f869f4e36c
Xiyuan Liu <xliu1@tcd.ie>
2024-06-09 15:47:06 +0100

    Fixing A Major Google Auth Issue
    Retrieve the user document reference using doc.
    
    Retrieve the user document snapshot using getDoc.
    
    If the user document exists, fetch the existing user data.
    
    Store user information in localStorage.
    
    Call loginUser with the existing userDoc.

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -3,7 +3,7 @@ import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
 import { auth, firestore } from '../../firebase/firebase'
 import useShowToast from '../../hooks/useShowToast'
 import useAuthStore from '../../store/authStore'
-import { doc, setDoc } from 'firebase/firestore'
+import { doc, getDoc, setDoc } from 'firebase/firestore'
 
 const GoogleAuth = ({prefix}) => {
   const [signInWithGoogle, , , error] = useSignInWithGoogle(auth)
@@ -17,7 +17,13 @@ const GoogleAuth = ({prefix}) => {
         showToast("Error", error.message, "error")
         return
       }
-      if (newUser) {
+      const userRef = doc(firestore, "users", newUser.user.uid)
+      const userSnap = await getDoc(userRef)
+      if (userSnap.exists()) {
+        const userDoc = userSnap.data()
+        localStorage.setItem("user-info", JSON.stringify(userDoc))
+        loginUser(userDoc)
+      } else {
         const userDoc = {
           uid: newUser.user.uid,
           email: newUser.user.email,
