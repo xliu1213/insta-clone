a5adfb2fa066dd4944d9b71e214819a2932e7f0d
Xiyuan Liu <xliu1@tcd.ie>
2024-06-12 23:25:43 +0100

    Fetching Profile Header Data (Part VI)
    authUser gets the authenticated user from the useAuthStore.
    
    visitingOwnProfileAndAuth checks if the authenticated user exists and if their username matches the username of the profile being viewed.
    
    visitingAnotherProfileAndAuth checks if the authenticated user exists and that their username does not match the username of the profile being viewed.
    
    Conditionally renders either the "Edit Profile" button or the "Follow" button.

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,8 +1,12 @@
 import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
 import useUserProfileStore from "../../store/userProfileStore"
+import useAuthStore from '../../store/authStore'
 
 const ProfileHeader = () => {
   const {userProfile} = useUserProfileStore()
+  const authUser = useAuthStore(state => state.user)
+  const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username
+  const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username
 
   return <Flex gap={{base:4,sm:10}} py={10} direction={{base:"column", sm:"row"}}>
     <AvatarGroup size={{base:"xl",md:"2xl"}} mx={"auto"}>
@@ -11,9 +15,16 @@ const ProfileHeader = () => {
     <VStack alignItems={"start"} flex={1}>
       <Flex gap={4} direction={{base:"column", sm:"row"}} alignItems={"center"} w={"full"}>
           <Text fontSize={{base:"sm",md:"lg"}}>{userProfile.username}</Text>
-          <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
-            Edit Profile
-          </Button>
+          {visitingOwnProfileAndAuth && (
+            <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
+              Edit Profile
+            </Button>
+          )}
+          {visitingAnotherProfileAndAuth && (
+            <Button bg={"blue.500"} color={"white"} _hover={{bg:"blue.600"}} size={{base:"xs",md:"sm"}}>
+              Follow
+            </Button>
+          )}
       </Flex>
       <Flex gap={{base:2, sm:4}} fontSize={{base:"xs", md:"sm"}}>
         <Text><Text as={"span"} fontWeight={"bold"} mr={1}>{userProfile.posts.length}</Text> Posts</Text>
