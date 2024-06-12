3e6cc5be64138223f99c6a7647dceeb7592abfcd
Xiyuan Liu <xliu1@tcd.ie>
2024-06-12 14:40:36 +0100

    Fetching Profile Header Data (Part IV) Render UserNotFound if the user profile is not found. Render ProfileHeader or ProfileHeaderSkeleton based on the loading state. ProfileHeaderSkeleton is a Flex containing a SkeletonCircle and a VStack. UserNotFound is a Flex containing a Text and a Link.

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,17 +1,20 @@
-import { Container, Flex } from "@chakra-ui/react"
+import { Container, Flex, Text, Link, SkeletonCircle, VStack, Skeleton } from "@chakra-ui/react"
 import ProfileHeader from "../../components/Profile/ProfileHeader"
 import ProfileTabs from "../../components/Profile/ProfileTabs"
 import ProfilePosts from "../../components/Profile/ProfilePosts"
-import { useParams } from "react-router-dom"
+import { Link as RouterLink, useParams } from "react-router-dom"
 import useGetUserProfileByUsername from "../../hooks/useGetUserProfileByUsername"
 
 const ProfilePage = () => {
   const {username} = useParams()
   const {isLoading, userProfile} = useGetUserProfileByUsername(username)
+  const userNotFound = !isLoading && !userProfile
+  if (userNotFound) return <UserNotFound />
 
   return <Container maxW={"container.lg"} py={5}>
     <Flex py={10} px={4} pl={{base:4, md:10}} w={"full"} mx={"auto"} flexDirection={"column"}>
-      <ProfileHeader />
+      {!isLoading && userProfile && <ProfileHeader />}
+      {isLoading && <ProfileHeaderSkeleton />}
     </Flex>
     <Flex px={{base:2, sm:4}} maxW={"full"} mx={"auto"} borderTop={"1px solid"} borderColor={"whiteAlpha.300"} 
       flexDirection={"column"}>
@@ -22,3 +25,25 @@ const ProfilePage = () => {
 }
 
 export default ProfilePage
+
+// skeleton for profile header
+const ProfileHeaderSkeleton = () => {
+  return (
+    <Flex gap={{base:4, sm:10}} py={10} direction={{base:"column", sm:"row"}} justifyContent={"center"} alignItems={"center"}>
+      <SkeletonCircle size={24} />
+      <VStack alignItems={{base:"center", sm:"flex-start"}} gap={2} mx={"auto"} flex={1}>
+        <Skeleton height={"12px"} width={"150px"} />
+        <Skeleton height={"12px"} width={"100px"} />
+      </VStack>
+    </Flex>
+  )
+}
+
+const UserNotFound = () => {
+  return (
+    <Flex flexDir={"column"} textAlign={"center"} mx={"auto"}>
+      <Text fontSize={"2xl"}>User Not Found</Text>
+      <Link as={RouterLink} to={"/"} color={"blue.500"} w={"max-content"} mx={"auto"}>Go home</Link>
+    </Flex>
+  )
+}
