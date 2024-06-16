34b27fab830158d66b5e9dcf445d036ab4b17ac4
Xiyuan Liu <xliu1@tcd.ie>
2024-06-16 01:08:06 +0100

    Edit User Profile (Part V)
    Check if the selected file is an image and if the file size exceeds a maximum limit of 2MB.
    Else an error toast is shown and selectedFile is set to null.
    A FileReader instance is created to read the file as a data URL.
    The reader.onloadend event handler sets the selectedFile state with the base64 string of the image when the file reading is complete.
    The usePreviewImg hook returns an object that allows access to the selected image, handle file changes, and reset the selected file.

--- src/hooks/usePreviewImg.js
+++ src/hooks/usePreviewImg.js
@@ -8,7 +8,24 @@ const usePreviewImg = () => {
 
   const handleImageChange = (e) => {
     const file = e.target.files[0]
+    if (file && file.type.startsWith("image/")) {
+      if (file.size > maxFileSizeInBytes) {
+        showToast("Error", "File size must be less than 2MB", "error")
+        setSelectedFile(null)
+        return
+      }
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setSelectedFile(reader.result)
+      }
+      reader.readAsDataURL(file)
+    } else {
+      showToast("Error", "Please select an image file", "error")
+      setSelectedFile(null)
+    }
   }
+
+  return {selectedFile, handleImageChange, setSelectedFile}
 }
 
 export default usePreviewImg
