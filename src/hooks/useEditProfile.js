8d389674707d7435e2bb6ec7cb4378df18432682
Xiyuan Liu <xliu1@tcd.ie>
2024-06-20 11:20:45 +0100

    Edit User Profile (Part VII)
    Creates a reference to the storage location for the profile picture.
    Creates a reference to the user document in Firestore.
    Initializes a URL variable for storing the profile picture URL.
    A try-catch block handles errors during the upload and update process.
    If selectedFile is provided, it uploads the file to Firebase Storage using uploadString.
    Retrieves the download URL of the uploaded file using getDownload URL.
    Constructs an updatedUser object with updated fields.

--- src/hooks/useEditProfile.js
+++ src/hooks/useEditProfile.js
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import useAuthStore from "../store/authStore"
 import useShowToast from "./useShowToast"
+import { getDownloadURL, ref, uploadString } from "firebase/storage"
+import { firestore, storage } from '../firebase/firebase'
+import { doc } from "firebase/firestore"
 
 const useEditProfile = () => {
   const [isUpdating, setIsUpdating] = useState(false)
@@ -10,6 +13,24 @@ const useEditProfile = () => {
   const editProfile = async (inputs, selectedFile) => {
     if (isUpdating || !authUser) return 
     setIsUpdating(true)
+    const storageRef = ref(storage, `profilePics/${authUser.uid}`)
+    const userDocRef = doc(firestore, "users", authUser.uid)
+    let URL = ""
+    try {
+      if (selectedFile) {
+        await uploadString(storageRef, selectedFile, "data_url")
+        URL = await getDownloadURL(ref(storage, `profilePics/${authUser.uid}`))
+      }
+      const updatedUser = {
+        ...authUser,
+        fullName: inputs.fullName || authUser.fullName,
+        username: inputs.username || authUser.username,
+        bio: inputs.bio || authUser.bio,
+        profilePicURL: URL || authUser.profilePicURL,
+      }
+    } catch (error) {
+      showToast("Error", error.message, "error")
+    }
   }
 }
 
