aa496872b37bc1b57085f8c07150e9b9037883b8
Xiyuan Liu <xliu1@tcd.ie>
2024-06-24 15:59:28 +0100

    Search For User Profiles (Part III) The useSearchUser hook provides user search functionality with user, isLoading and getUserProfile. handleSearchUser function handles form submission for user search. A Modal contains ModalContent containing a ModalHeader, ModalCloseButton and ModalBody. The ModalBody contains a form containing a FormControl and Flex. The FormControl contains a FormLabel and Input. The Flex contains a Button.

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -1,8 +1,27 @@
-import { Flex, Tooltip, Box, useDisclosure } from "@chakra-ui/react"
+import { 
+  Flex, 
+  Tooltip, 
+  Box, 
+  useDisclosure, 
+  Modal, 
+  ModalContent, 
+  ModalHeader, 
+  ModalCloseButton, 
+  ModalBody, 
+  FormControl, 
+  FormLabel, 
+  Input, 
+  Button 
+} from "@chakra-ui/react"
 import { SearchLogo } from "../../assets/constants"
+import useSearchUser from '../../hooks/useSearchUser'
 
 const Search = () => {
   const {isOpen, onOpen, onClose} = useDisclosure()
+  const {user, isLoading, getUserProfile} = useSearchUser()
+  const handleSearchUser = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <>
@@ -13,6 +32,23 @@ const Search = () => {
             <Box display={{base:"none", md:"block"}}>Search</Box>
         </Flex>
       </Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInLeft">
+        <ModalContent bg={"black"} border={"1px solid gray"} maxW={"400px"}>
+          <ModalHeader>Search user</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <form onSubmit={handleSearchUser}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input placeholder="asaprogrammer" ref={searchRef} />
+              </FormControl>
+              <Flex w={"full"} justifyContent={"flex-end"}>
+                <Button type="submit" ml={"auto"} size={"sm"} my={4} isLoading={isLoading}>Search</Button>
+              </Flex>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
