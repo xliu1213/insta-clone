463b94e540985fc5e2bb4bd17f1e353a4c7c00b3
Xiyuan Liu <xliu1@tcd.ie>
2024-06-25 17:18:59 +0100

    Create Post (Part I) useDisclosure hook handles the modal's open and close state. Added onClick event to open the modal when the Flex container is clicked. Modal contains a ModalOverlay and ModalContent. ModalContent contains ModalHeader, ModalCloseButton, ModalBody, ModalFooter. ModalBody contains Textarea, Input, BsFillImageFill. ModalFooter contains Button.

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -1,15 +1,50 @@
+import { BsFillImageFill } from "react-icons/bs"
 import { CreatePostLogo } from "../../assets/constants"
-import { Tooltip, Flex, Box } from "@chakra-ui/react"
+import { 
+  Tooltip, 
+  Flex, 
+  Box, 
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Textarea,
+  Input
+} from "@chakra-ui/react"
 
 const CreatePost = () => {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+
   return (
-    <Tooltip hasArrow label={"Create"} placement="right" ml={1} openDelay={500} display={{base:"block", md:"none"}}>
-      <Flex alignItems={"center"} gap={4} _hover={{bg:"whiteAlpha.400"}} borderRadius={6} p={2} w={{base:10, md:"full"}} 
-        justifyContent={{base:"center", md:"flex-start"}}>
-          <CreatePostLogo />
-          <Box display={{base:"none", md:"block"}}>Create</Box>
-      </Flex>
-    </Tooltip>
+    <>
+      <Tooltip hasArrow label={"Create"} placement="right" ml={1} openDelay={500} display={{base:"block", md:"none"}}>
+        <Flex alignItems={"center"} gap={4} _hover={{bg:"whiteAlpha.400"}} borderRadius={6} p={2} w={{base:10, md:"full"}} 
+          justifyContent={{base:"center", md:"flex-start"}} onClick={onOpen}>
+            <CreatePostLogo />
+            <Box display={{base:"none", md:"block"}}>Create</Box>
+        </Flex>
+      </Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} size={"xl"}>
+        <ModalOverlay />
+        <ModalContent bg={"black"} border={"1px solid gray"}>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Textarea placeholder="Post caption..." />
+            <Input type="file" hidden />
+            <BsFillImageFill style={{marginTop:"15px", marginLeft:"5px", cursor:"pointer"}} />
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={3}>Post</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
