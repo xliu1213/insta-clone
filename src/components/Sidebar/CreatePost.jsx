4c0eb35770a9414b734825a09c514b67ad430236
Xiyuan Liu <xliu1@tcd.ie>
2024-06-25 23:39:48 +0100

    Create Post (Part II)
    The useState hook creates a caption state variable to manage the text entered in the Textarea component.
    The useRef hook creates an imageRef for the file input element.
    The usePreviewImg hook manages image selection and preview functionality.
    It provides handleImageChange, selectedFile for storing the selected file URL and setSelectedFile.
    Textarea now has value and onChange properties to bind it to the caption state variable, allowing the caption text to be updated as the user types.
    Input is updated with a ref attribute and an onChange event handler to process file selection.
    The BsFillImageFill icon includes an onClick event that triggers a click on the hidden file input.
    If selectedFile is not empty, a Flex containing an Image and CloseButton is rendered.
    Clicking the CloseButton will clear the selectedFile state.

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -14,11 +14,18 @@ import {
   ModalFooter,
   Button,
   Textarea,
-  Input
+  Input,
+  Image,
+  CloseButton
 } from "@chakra-ui/react"
+import { useRef, useState } from "react"
+import usePreviewImg from '../../hooks/usePreviewImg'
 
 const CreatePost = () => {
   const {isOpen, onOpen, onClose} = useDisclosure()
+  const [caption, setCaption] = useState("")
+  const imageRef = useRef(null)
+  const {handleImageChange, selectedFile, setSelectedFile} = usePreviewImg()
 
   return (
     <>
@@ -35,9 +42,16 @@ const CreatePost = () => {
           <ModalHeader>Create Post</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <Textarea placeholder="Post caption..." />
-            <Input type="file" hidden />
-            <BsFillImageFill style={{marginTop:"15px", marginLeft:"5px", cursor:"pointer"}} />
+            <Textarea placeholder="Post caption..." value={caption} onChange={(e) => setCaption(e.target.value)} />
+            <Input type="file" hidden ref={imageRef} onChange={handleImageChange} />
+            <BsFillImageFill style={{marginTop:"15px", marginLeft:"5px", cursor:"pointer"}}
+              onClick={() => imageRef.current.click()} />
+            {selectedFile && (
+              <Flex mt={5} w={"full"} position={"relative"} justifyContent={"center"}>
+                <Image src={selectedFile} alt="Selected img" />
+                <CloseButton position={"absolute"} top={2} right={2} onClick={() => setSelectedFile("")} />
+              </Flex>
+            )}
           </ModalBody>
           <ModalFooter>
             <Button mr={3}>Post</Button>
