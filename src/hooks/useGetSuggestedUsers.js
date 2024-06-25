5f4898b1e1bb33dc41e48604b5e385b58fdbf4e9
Xiyuan Liu <xliu1@tcd.ie>
2024-06-25 13:44:20 +0100

    Suggested Users (Part II)
    Set isLoading state before data fetch.
    Process the fetched data and update the suggestedUsers state.
    Use a finally block to guarantee that the loading state is correctly updated.
    getSuggestedUsers is called only if authUser is defined.
    The dependency array of useEffect ensures that the effect re-runs whenever authUser and showToast changes.
    Return the isLoading and suggestedUsers state variables from the hook.

--- src/hooks/useGetSuggestedUsers.js
+++ src/hooks/useGetSuggestedUsers.js
@@ -12,15 +12,25 @@ const useGetSuggestedUsers = () => {
 
   useEffect(() => {
     const getSuggestedUsers = async () => {
+      setIsLoading(true)
       try {
         const usersRef = collection(firestore, "users")
         const q = query(usersRef, where("uid", "not-in", [authUser.uid, ...authUser.following]), orderBy("uid"), limit(3))
         const querySnapshot = await getDocs(q)
+        const users = []
+        querySnapshot.forEach(doc => {users.push({...doc.data(), id: doc.id})})
+        setSuggestedUsers(users)
       } catch (error) {
         showToast("Error", error.message, "error")
+      } finally {
+        setIsLoading(false)
       }
     }
-  }, [])
+
+    if (authUser) getSuggestedUsers()
+  }, [authUser, showToast])
+
+  return {isLoading, suggestedUsers}
 }
 
 export default useGetSuggestedUsers
