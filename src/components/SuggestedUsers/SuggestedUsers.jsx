6a74213b99cb0617732846bf196f9986c2ec09a9
Xiyuan Liu <xliu1@tcd.ie>
2024-06-25 15:57:45 +0100

    Suggested Users (Part III)
    SuggestedUsers.jsx uses a custom hook useGetSuggestedUsers to fetch the list of suggested users dynamically.
    If the data is still loading, it returns null.
    Maps over the suggestedUsers array to render SuggestedUser components dynamically.
    
    HomePage.jsx uncomments out SuggestedUsers.

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,17 +1,21 @@
 import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
 import SuggestedUser from "./SuggestedUser"
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers"
 
 const SuggestedUsers = () => {
+  const {isLoading, suggestedUsers} = useGetSuggestedUsers()
+  if (isLoading) return null
+
   return <VStack py={8} px={6} gap={4}>
     <SuggestedHeader/>
     <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
       <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>Suggested for you</Text>
       <Text fontSize={12} fontWeight={"bold"} _hover={{color:"gray.400"}} cursor={"pointer"}>See All</Text>
     </Flex>
-    <SuggestedUser name='Dan Abramov' followers={1392} avatar='https://bit.ly/dan-abramov'/>
-    <SuggestedUser name='Ryan Florence' followers={567} avatar='https://bit.ly/ryan-florence'/>
-    <SuggestedUser name='Christian Nwamba' followers={759} avatar='https://bit.ly/code-beast'/>
+    {suggestedUsers.map(user => (
+      <SuggestedUser user={user} key={user.id} />
+    ))}
     <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
       © 2024 Built By{" "}
       <Link href="https://github.com/xliu1213/insta-clone" target="_blank" color={"blue.500"} fontSize={14}>
