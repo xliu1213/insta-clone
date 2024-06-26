2743029c807f1c40e168c6d002a3edf5c04af02c
Xiyuan Liu <xliu1@tcd.ie>
2024-06-26 23:52:42 +0100

    Fetch User Posts (Part IV) userProfile and authUser are fetched. The Image is wrapped in a Flex and centered. The Avatar and Text components dynamically display the userProfile data. The delete button is only shown if the authUser is the same as the userProfile. The delete icon is wrapped in a small, transparent Button. Most comments are removed.

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,13 +1,17 @@
-import { Avatar, Box, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, 
+import { Avatar, Box, Button, Divider, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, Text, 
   VStack, useDisclosure } from "@chakra-ui/react"
 import { AiFillHeart } from "react-icons/ai"
 import { FaComment } from "react-icons/fa"
 import { MdDelete } from "react-icons/md"
 import Comment from "../Comment/Comment"
 import PostFooter from "../FeedPosts/PostFooter"
+import useUserProfileStore from "../../store/userProfileStore"
+import useAuthStore from '../../store/authStore'
 
 const ProfilePost = ({post}) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const userProfile = useUserProfileStore(state => state.userProfile)
+  const authUser = useAuthStore(state => state.user)
 
   return <>
     <GridItem cursor={"pointer"} borderRadius={4} overflow={"hidden"} border={"1px solid whiteAlpha.300"}
@@ -33,35 +37,24 @@ const ProfilePost = ({post}) => {
         <ModalCloseButton />
         <ModalBody bg={"black"} pb={5}>
           <Flex gap={4} w={{base:"90%", sm:"70%", md:"full"}} mx={"auto"}>
-            <Box flex={1.5} borderRadius={4} overflow={"hidden"} border={"1px solid whiteAlpha.300"}>
-              <Image src={post.imageURL} alt="profile post" />
-            </Box>
+            <Flex flex={1.5} borderRadius={4} overflow={"hidden"} border={"1px solid whiteAlpha.300"} justifyContent={"center"}
+              alignItems={"center"}>
+                <Image src={post.imageURL} alt="profile post" />
+            </Flex>
             <Flex flex={1} flexDirection={"column"} px={10} display={{base:"none", md:"flex"}}>
               <Flex alignItems={"center"} justifyContent={"space-between"}>
                 <Flex alignItems={"center"} gap={4}>
-                  <Avatar src="/profilepic.png" size={"sm"} name="As a Programmer"/>
-                  <Text fontWeight={"bold"} fontSize={12}>xiyuan</Text>
+                  <Avatar src={userProfile.profilePicURL} size={"sm"} name="As a Programmer"/>
+                  <Text fontWeight={"bold"} fontSize={12}>{userProfile.username}</Text>
                 </Flex>
-                <Box _hover={{bg:"whiteAlpha.300", color:"red.600"}} borderRadius={4} p={1}>
-                  <MdDelete size={20} cursor={"pointer"} />
-                </Box>
+                {authUser?.uid === userProfile.uid && (
+                  <Button _hover={{bg:"whiteAlpha.300", color:"red.600"}} borderRadius={4} p={1} size={"sm"} bg={"transparent"}>
+                    <MdDelete size={20} cursor={"pointer"} />
+                  </Button>
+                )}
               </Flex>
               <Divider my={4} bg={"gray.500"}/>
               <VStack w={"full"} alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
-                <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
-                <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
-                <Comment createdAt="3h ago" username="kentdodds" profilePic="https://bit.ly/kent-c-dodds" 
-                  text={"Good clone dude!"}/>
-                <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
-                <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
-                <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
-                <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
-                <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
-                <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
-                <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
-                <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
-                <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
-                <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
                 <Comment createdAt="1d ago" username="xiyuan" profilePic="/profilepic.png" text={"Dummy images from unsplash"}/>
                 <Comment createdAt="12h ago" username="abrahmov" profilePic="https://bit.ly/dan-abramov" text={"Nice pic"}/>
               </VStack>
