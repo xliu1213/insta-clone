e16b40acaf84a6e3edec3c19cbdcc0cd236f1294
Xiyuan Liu <xliu1@tcd.ie>
2024-06-27 22:06:16 +0100

    Create Comment (Part II)
    usePostComment.js:
    addComment is a function extracted from usePostStore to update the local state after a comment is added.
    Update the Firestore document of the post by appending the new comment to the comments array.
    Calls addComment, showToast, return isCommenting and handlePostComment.
    
    postStore.js:
    The addComment function takes a postId and a comment and updates the relevant post in the state by appending the new comment to the comments array of the post.
    Uses set to update the state, mapping over the posts array to find the correct post by postId and updating its comments array.

--- src/hooks/usePostComment.js
+++ src/hooks/usePostComment.js
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import useShowToast from './useShowToast'
 import useAuthStore from '../store/authStore'
+import usePostStore from "../store/postStore"
+import { arrayUnion, doc, updateDoc } from "firebase/firestore"
+import { firestore } from '../firebase/firebase'
 
 const usePostComment = () => {
   const [isCommenting, setIsCommenting] = useState(false)
   const showToast = useShowToast()
   const authUser = useAuthStore(state => state.user)
+  const addComment = usePostStore(state => state.addComment)
 
   const handlePostComment = async (postId, comment) => {
     if (isCommenting) return
@@ -18,13 +22,16 @@ const usePostComment = () => {
       postId,
     }
     try {
-      
+      await updateDoc(doc(firestore, "posts", postId), {comments: arrayUnion(newComment)})
+      addComment(postId, newComment)
     } catch (error) {
-      
+      showToast("Error", error.message, "error")
     } finally {
       setIsCommenting(false)
     }
   }
+
+  return {isCommenting, handlePostComment}
 }
 
 export default usePostComment
