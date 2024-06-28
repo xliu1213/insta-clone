12bd63d0368f9d57d3a0c4b993d4d7e88545b41e
Xiyuan Liu <xliu1@tcd.ie>
2024-06-28 19:08:20 +0100

    Like And Unlike Posts (Part II)
    useLikePost.js:
    Update the likes array in the Firestore document for the post.
    If the post is already liked by the user, the user's UID is removed from the array.
    Else, the user's UID is added to the array.
    The local state is updated to reflect the new like status.
    The likes state is incremented or decremented based on the like status.
    The hook returns the isLiked status, the likes count, the handleLikePost function, and the isUpdating state.
    
    PostFooter.jsx:
    Replaced the previous state and handler for likes with the useLikePost hook.
    Updated the onClick handler for the like button.

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -3,34 +3,24 @@ import { useRef, useState } from "react"
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from '../../assets/constants'
 import useAuthStore from "../../store/authStore"
 import usePostComment from "../../hooks/usePostComment"
+import useLikePost from "../../hooks/useLikePost"
 
 const PostFooter = ({post, username, isProfilePage}) => {
-  const [liked, setLiked] = useState(false)
-  const [likes, setLikes] = useState(1000)
   const {isCommenting, handlePostComment} = usePostComment()
   const [comment, setComment] = useState("")
   const authUser = useAuthStore(state => state.user)
   const commentRef = useRef(null)
+  const {isLiked, likes, handleLikePost} = useLikePost(post)
 
   const handleSubmitComment = async () => {
     await handlePostComment(post.id, comment)
     setComment("")
   }
 
-  const handleLike = () => {
-    if(liked) {
-      setLiked(false)
-      setLikes(likes - 1)
-    } else {
-      setLiked(true)
-      setLikes(likes + 1)
-    }
-  }
-
   return <Box mb={10} mt={"auto"}>
     <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
-      <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
-        {!liked ? (<NotificationsLogo />) : (<UnlikeLogo />)}
+      <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+        {!isLiked ? (<NotificationsLogo />) : (<UnlikeLogo />)}
       </Box>
       <Box cursor={"pointer"} fontSize={18} onClick={() => commentRef.current.focus()}><CommentLogo /></Box>
     </Flex>
--- src/hooks/useLikePost.js
+++ src/hooks/useLikePost.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import useAuthStore from '../store/authStore'
 import useShowToast from './useShowToast'
-import { doc } from "firebase/firestore"
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore"
 import { firestore } from '../firebase/firebase'
 
 const useLikePost = (post) => {
@@ -17,12 +17,17 @@ const useLikePost = (post) => {
     setIsUpdating(true)
     try {
       const postRef = doc(firestore, "posts", post.id)
+      await updateDoc(postRef, {likes: isLiked ? arrayRemove(authUser.uid) : arrayUnion(authUser.uid)})
+      setIsLiked(!isLiked)
+      isLiked ? setLikes(likes - 1) : setLikes(likes + 1)
     } catch (error) {
       showToast("Error", error.message, "error")
     } finally {
       setIsUpdating(false)
     }
   }
+
+  return {isLiked, likes, handleLikePost, isUpdating}
 }
 
 export default useLikePost
