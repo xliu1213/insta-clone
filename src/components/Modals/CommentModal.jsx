807dde4aa1e3712a843c9ecd75b6adb6da27a7f2
Xiyuan Liu <xliu1@tcd.ie>
2024-06-30 16:09:47 +0100

    Fetch All Comments (Part II) The CommentModal accepts a post prop, which contains the details of the post including its comments. Destructures handlePostComment and isCommenting from the usePostComment hook. A reference is created for the comment input field. The handleSubmitComment function prevents the default form submission. Calls handlePostComment with the post ID and the comment value, and then clears the input field. A Flex container is added to display the list of comments. Each comment is mapped to a Comment component. The form uses handleSubmitComment for the onSubmit event, and the input field is connected to commentRef. The Button component includes the isLoading prop.

--- src/components/Modals/CommentModal.jsx
+++ src/components/Modals/CommentModal.jsx
@@ -1,6 +1,17 @@
 import { Button, Flex, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay } from "@chakra-ui/react"
+import usePostComment from '../../hooks/usePostComment'
+import { useRef } from "react"
+import Comment from '../Comment/Comment'
 
-const CommentModal = ({isOpen, onClose}) => {
+const CommentModal = ({isOpen, onClose, post}) => {
+  const {handlePostComment, isCommenting} = usePostComment()
+  const commentRef = useRef(null)
+  const handleSubmitComment = async (e) => {
+    e.preventDefault()
+    await handlePostComment(post.id, commentRef.current.value)
+    commentRef.current.value = ''
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInLeft">
       <ModalOverlay />
@@ -8,10 +19,15 @@ const CommentModal = ({isOpen, onClose}) => {
         <ModalHeader>Comments</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
-          <form style={{marginTop:"2rem"}}>
-            <Input placeholder="Comment" size={"sm"} />
+          <Flex mb={4} gap={4} flexDir={"column"} maxH={"250px"} overflowY={"auto"}>
+            {post.comments.map((comment, idx) => (
+              <Comment key={idx} comment={comment} />
+            ))}
+          </Flex>
+          <form style={{marginTop:"2rem"}} onSubmit={handleSubmitComment}>
+            <Input placeholder="Comment" size={"sm"} ref={commentRef} />
             <Flex w={"full"} justifyContent={"flex-end"}>
-              <Button type="submit" ml={"auto"} size={"sm"} my={4}>Post</Button>
+              <Button type="submit" ml={"auto"} size={"sm"} my={4} isLoading={isCommenting}>Post</Button>
             </Flex>
           </form>
         </ModalBody>
