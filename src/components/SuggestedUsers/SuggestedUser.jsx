1c9918230d7ea17733c94867836d9bb6add0346d
Xiyuan Liu <xliu1@tcd.ie>
2024-06-30 17:27:46 +0100

    Suggested users link back from home page

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,6 +1,7 @@
 import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react"
 import useFollowUser from '../../hooks/useFollowUser'
 import useAuthStore from "../../store/authStore"
+import { Link } from "react-router-dom"
 
 const SuggestedUser = ({user, setUser}) => {
   const {isUpdating, isFollowing, handleFollowUser} = useFollowUser(user.uid)
@@ -16,9 +17,13 @@ const SuggestedUser = ({user, setUser}) => {
 
   return <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
     <Flex alignItems={"center"} gap={2}>
-      <Avatar src={user.profilePicURL} size={"md"}/>
+      <Link to={`/${user.username}`}>
+        <Avatar src={user.profilePicURL} size={"md"}/>
+      </Link>
       <VStack spacing={2} alignItems={"flex-start"}>
-        <Box fontSize={12} fontWeight={"bold"}>{user.fullName}</Box>
+        <Link to={`/${user.username}`}>
+          <Box fontSize={12} fontWeight={"bold"}>{user.fullName}</Box>
+        </Link>
         <Box fontSize={12} color={"gray.500"}>{user.followers.length} followers</Box>
       </VStack>
     </Flex>
